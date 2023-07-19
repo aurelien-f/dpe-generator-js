a985f586fb6c2dba6e4943cd28a4ad638720f639
aurelien-mk <43632571+aurelien-mk@users.noreply.github.com>
2023-07-19 09:55:50 +0200

    build

--- dist/dpe-generator.js
+++ dist/dpe-generator.js
@@ -1,19 +1,19 @@
-const z = document.querySelectorAll(".dpe-generator-js");
-let A = 10, L = 29, G = 14, S = 65, t = 35, s = 40, _ = {
-  a: t,
-  b: t + s,
-  c: t + s * 2,
-  d: t + s * 3,
-  e: t + s * 4,
-  f: t + s * 5,
-  g: t + s * 6
+const A = document.querySelectorAll(".dpe-generator-js");
+let L = 10, G = 29, S = 14, _ = 65, E = 70, s = 35, n = 40, T = {
+  a: s,
+  b: s + n,
+  c: s + n * 2,
+  d: s + n * 3,
+  e: s + n * 4,
+  f: s + n * 5,
+  g: s + n * 6
 };
 function c(e) {
-  const l = e.getAttribute("dpe-consommation"), m = e.getAttribute("dpe-emissions"), n = e.getAttribute("dpe-letter").toLowerCase(), d = e.getAttribute("dpe-type"), o = e.offsetWidth / 500, u = o * A, g = o * L, f = o * G, $ = o * S, b = o * _[n], i = `style="font-size: ${u}px"`, a = `style="font-size: ${f}px"`, r = `style="font-size: ${g}px"`, p = `style="height: ${$}px"`, y = `style="top: ${b}px"`, h = "./img/energie/" + n + ".svg", v = new URL(h, import.meta.url).href, x = n !== "f" && n !== "g" ? `<div class="dpeGenerator_passoire"><p ${a}>passoire<br> énergetique</p></div>` : "", w = `<figure class="dpeGenerator dpeGenerator-${d}">
-    ${x}
+  const l = e.getAttribute("dpe-consommation"), m = e.getAttribute("dpe-emissions"), o = e.getAttribute("dpe-letter").toLowerCase(), d = e.getAttribute("dpe-type"), t = e.offsetWidth / 500, u = t * L, $ = t * G, g = t * S, f = t * _, b = t * T[o], y = t * E, i = `style="font-size: ${u}px"`, a = `style="font-size: ${g}px"`, r = `style="font-size: ${$}px"`, p = `style="height: ${f}px"`, h = `style="top: ${b}px"`, v = `style="bottom: ${y}px"`, x = "./img/energie/" + o + ".svg", w = new URL(x, import.meta.url).href, z = o !== "f" && o !== "g" ? `<div ${v} class="dpeGenerator_passoire"><p ${a}>passoire<br> énergetique</p></div>` : "", P = `<figure class="dpeGenerator dpeGenerator-${d}">
+    ${z}
     <div class="dpeGenerator_text"><p ${a}>Logement extrêmement performant</p><p ${a}>Logement extrêmement peu performant</p></div>
-    <img id="dpeImg" src="${v}" alt="étiquette energie dpe ${n}">
-    <div ${y} class="dpeGenerator_infos">
+    <img id="dpeImg" src="${w}" alt="étiquette energie dpe ${o}">
+    <div ${h} class="dpeGenerator_infos">
     <div class="dpeGenerator_infos_consommation">
         <span ${i} class="top alt">consommation</span>
         <span ${p} class="bot">
@@ -30,9 +30,9 @@ function c(e) {
     </div>
     </div>
     </figure>`;
-  e.innerHTML = w;
+  e.innerHTML = P;
 }
-z.forEach((e) => {
+A.forEach((e) => {
   c(e);
 });
 window.addEventListener("resize", c);
--- dist/dpe-generator.umd.cjs
+++ dist/dpe-generator.umd.cjs
@@ -1,21 +1,21 @@
-(function(r){typeof define=="function"&&define.amd?define(r):r()})(function(){"use strict";const r="",l=document.querySelectorAll(".dpe-generator-js");let m=10,u=29,f=14,g=65,t=35,s=40,$={a:t,b:t+s,c:t+s*2,d:t+s*3,e:t+s*4,f:t+s*5,g:t+s*6};function c(e){const b=e.getAttribute("dpe-consommation"),y=e.getAttribute("dpe-emissions"),n=e.getAttribute("dpe-letter").toLowerCase(),h=e.getAttribute("dpe-type"),o=e.offsetWidth/500,v=o*m,w=o*u,x=o*f,z=o*g,L=o*$[n],i=`style="font-size: ${v}px"`,a=`style="font-size: ${x}px"`,p=`style="font-size: ${w}px"`,d=`style="height: ${z}px"`,S=`style="top: ${L}px"`,_="./img/energie/"+n+".svg",A=new URL(_,typeof document>"u"&&typeof location>"u"?require("url").pathToFileURL(__filename).href:typeof document>"u"?location.href:document.currentScript&&document.currentScript.src||new URL("dpe-generator.umd.cjs",document.baseURI).href).href,G=n!=="f"&&n!=="g"?`<div class="dpeGenerator_passoire"><p ${a}>passoire<br> énergetique</p></div>`:"",E=`<figure class="dpeGenerator dpeGenerator-${h}">
-    ${G}
+(function(r){typeof define=="function"&&define.amd?define(r):r()})(function(){"use strict";const r="",d=document.querySelectorAll(".dpe-generator-js");let m=10,u=29,f=14,g=65,$=70,t=35,s=40,b={a:t,b:t+s,c:t+s*2,d:t+s*3,e:t+s*4,f:t+s*5,g:t+s*6};function c(e){const y=e.getAttribute("dpe-consommation"),h=e.getAttribute("dpe-emissions"),o=e.getAttribute("dpe-letter").toLowerCase(),v=e.getAttribute("dpe-type"),n=e.offsetWidth/500,w=n*m,x=n*u,z=n*f,L=n*g,P=n*b[o],S=n*$,i=`style="font-size: ${w}px"`,a=`style="font-size: ${z}px"`,p=`style="font-size: ${x}px"`,l=`style="height: ${L}px"`,_=`style="top: ${P}px"`,A=`style="bottom: ${S}px"`,G="./img/energie/"+o+".svg",E=new URL(G,typeof document>"u"&&typeof location>"u"?require("url").pathToFileURL(__filename).href:typeof document>"u"?location.href:document.currentScript&&document.currentScript.src||new URL("dpe-generator.umd.cjs",document.baseURI).href).href,T=o!=="f"&&o!=="g"?`<div ${A} class="dpeGenerator_passoire"><p ${a}>passoire<br> énergetique</p></div>`:"",U=`<figure class="dpeGenerator dpeGenerator-${v}">
+    ${T}
     <div class="dpeGenerator_text"><p ${a}>Logement extrêmement performant</p><p ${a}>Logement extrêmement peu performant</p></div>
-    <img id="dpeImg" src="${A}" alt="étiquette energie dpe ${n}">
-    <div ${S} class="dpeGenerator_infos">
+    <img id="dpeImg" src="${E}" alt="étiquette energie dpe ${o}">
+    <div ${_} class="dpeGenerator_infos">
     <div class="dpeGenerator_infos_consommation">
         <span ${i} class="top alt">consommation</span>
-        <span ${d} class="bot">
-            <span ${p} class="number number-consommation">${b}</span>
+        <span ${l} class="bot">
+            <span ${p} class="number number-consommation">${y}</span>
             <span ${i} class="alt">kWh/m<sub>2</sub>/an</span>
         </span>
     </div>
     <div class="dpeGenerator_infos_emissions">
         <span ${i} class="top alt">émissions</span>
-        <span ${d} class="bot">
-            <span ${p} class="number number-emissions">${y}<sub>*</sub></span>
+        <span ${l} class="bot">
+            <span ${p} class="number number-emissions">${h}<sub>*</sub></span>
             <span ${i} class="alt">kg CO<sub>2</sub>/m<sup>2</sup>/an</span>
         </span>
     </div>
     </div>
-    </figure>`;e.innerHTML=E}l.forEach(e=>{c(e)}),window.addEventListener("resize",c)});
+    </figure>`;e.innerHTML=U}d.forEach(e=>{c(e)}),window.addEventListener("resize",c)});
